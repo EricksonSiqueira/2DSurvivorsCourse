02726f22faff46252a60dd338cb1e7bd6516d755
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2024-01-14 01:01:19 -0300

    feat: add experience bar

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,14 +1,25 @@
 extends Node
 
-var current_exp = 0
+signal experience_updated(current_experience: float, target_experience: float)
+const TARGET_EXPERIENCE_GROWTH = 5
+
+var current_experience = 0
+var current_level = 1
+var target_experience = 5
 
 func _ready():
 	GameEvents.experience_dot_collected.connect(on_experience_dot_collected)
 	
 
 func increment_experience(number: float):
-	current_exp += number
-	print(current_exp)
+	current_experience = min(current_experience + number, target_experience)
+	experience_updated.emit(current_experience, target_experience)
+	
+	if (current_experience == target_experience):
+		current_level += 1
+		target_experience += TARGET_EXPERIENCE_GROWTH
+		current_experience = 0
+		experience_updated.emit(current_experience, target_experience)
 
 func on_experience_dot_collected(number: float):
 	increment_experience(number)
