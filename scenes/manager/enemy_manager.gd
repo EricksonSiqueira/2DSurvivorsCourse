08cb2b17cf58c312f1a63aa3af2a8cebd1f0c283
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2024-01-15 14:48:16 -0300

    feat: enemys only spawn in the arena

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -13,8 +13,36 @@ func _ready():
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
+
+
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if (player == null):
+		return Vector2.ZERO
 	
+	var number_of_rotation = 4
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+		var physics_ray_query_parameters = PhysicsRayQueryParameters2D.create(
+			player.global_position, spawn_position, 1)
+		
+		var intersect_ray_result = get_tree().root.world_2d.direct_space_state.intersect_ray(
+			physics_ray_query_parameters)
+		var does_it_not_intersect = intersect_ray_result.is_empty()
+		
+		if (does_it_not_intersect):
+			break
+		else:
+			var ninety_degrees_in_rad = deg_to_rad(90)
+			random_direction = random_direction.rotated(ninety_degrees_in_rad)
+	
+	return spawn_position
+
+
 func on_timer_timeout():
 	timer.start()
 	
@@ -22,16 +50,13 @@ func on_timer_timeout():
 	if (player == null):
 		return
 	
-	var random_spawn_point = Vector2.RIGHT.rotated(randf_range(0, TAU)) * SPAWN_RADIUS
-	var spawn_position = player.global_position + random_spawn_point
-	
 	#Cria inimigo
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	#Pega o nó pai, que nesse caso vai ser a main e adiciona um inimigo
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int):
