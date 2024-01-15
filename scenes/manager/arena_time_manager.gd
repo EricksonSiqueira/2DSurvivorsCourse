7b98ae29d4559201c6d8d5f270ae6280ea8d1995
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2024-01-15 09:27:52 -0300

    feat: adjust difficulty over time

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,11 +1,28 @@
 extends Node
 
+class_name ArenaTimeManager
+
+signal arena_difficulty_increased
+
+const DIFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer as Timer
 
+var arena_difficulty = 0
+
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta):
+	var next_time_target = timer.wait_time - ((arena_difficulty * 1) * DIFICULTY_INTERVAL)
+	
+	if (timer.time_left <= next_time_target):
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
+
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
