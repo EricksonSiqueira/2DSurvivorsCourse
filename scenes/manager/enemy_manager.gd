8326dd44e1c7261ca192df3f8cd37f0b58484eea
Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>
2024-01-16 22:10:49 -0300

    feat: add weight to enemys spawn

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,13 +3,16 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: ArenaTimeManager
 
 @onready var timer: Timer = $Timer
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
 
 func _ready():
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
@@ -51,7 +54,8 @@ func on_timer_timeout():
 		return
 	
 	#Cria inimigo
-	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var enemy_scene = enemy_table.pick_item()
+	var enemy = enemy_scene.instantiate() as Node2D
 	#Pega o nó pai, que nesse caso vai ser a main e adiciona um inimigo
 	
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
@@ -62,7 +66,10 @@ func on_timer_timeout():
 func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (.1 / 12) * arena_difficulty
 	time_off = min(time_off, .7)
-	print(time_off)
 	timer.wait_time = base_spawn_time - time_off
+	
+	if (arena_difficulty == 6):
+		print("entrou aqui >>>>>>>>>>>")
+		enemy_table.add_item(wizard_enemy_scene, 20)
 	
 
